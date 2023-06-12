16df69b48eb2b7c5bb667ee0b987b94fac961491
d.larin <larin.dn@icloud.com>
2023-06-12 21:11:34 +0400

    unique_in_order

--- kyu_6/unique_in_order.py
+++ kyu_6/unique_in_order.py
@@ -1,8 +1,10 @@
 def unique_in_order(sequence):
-    result = [sequence[1]]
+    result = []
+    if len(sequence) != 0:
+        result = [sequence[0]]
+        for s in sequence:
+            if s == result[-1]:
+                continue
+            result.append(s)
 
-    for s in sequence:
-        if s == result[-1]:
-            continue
-        result.append(s)
     return result
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from kyu_6.unique_in_order import unique_in_order
 
 
-print(unique_in_order('AAAABBBCCDAABBB'))
+print(unique_in_order('A'))
